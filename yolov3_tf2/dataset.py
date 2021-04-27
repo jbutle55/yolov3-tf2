cdd442faa23e590b9639f7f2d9bc3b35551aff5d
jbutle55 <justin.butler12@hotmail.ca>
2021-04-27 15:42:56 -0600

    Remove debug print

--- yolov3_tf2/dataset.py
+++ yolov3_tf2/dataset.py
@@ -111,7 +111,6 @@ def parse_tfrecord(tfrecord, class_table, size):
     class_text = tf.sparse.to_dense(
         x['image/object/class/text'], default_value='')
     labels = tf.cast(class_table.lookup(class_text), tf.float32)
-    print(labels)
     y_train = tf.stack([tf.sparse.to_dense(x['image/object/bbox/xmin']),
                         tf.sparse.to_dense(x['image/object/bbox/ymin']),
                         tf.sparse.to_dense(x['image/object/bbox/xmax']),
