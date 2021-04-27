67267ae49359496558d8d97cc7755c161a751cd1
jbutle55 <justin.butler12@hotmail.ca>
2021-04-27 15:23:45 -0600

    loss debugging

--- yolov3_tf2/dataset.py
+++ yolov3_tf2/dataset.py
@@ -111,6 +111,7 @@ def parse_tfrecord(tfrecord, class_table, size):
     class_text = tf.sparse.to_dense(
         x['image/object/class/text'], default_value='')
     labels = tf.cast(class_table.lookup(class_text), tf.float32)
+    print(labels)
     y_train = tf.stack([tf.sparse.to_dense(x['image/object/bbox/xmin']),
                         tf.sparse.to_dense(x['image/object/bbox/ymin']),
                         tf.sparse.to_dense(x['image/object/bbox/xmax']),
@@ -131,7 +132,7 @@ def load_tfrecord_dataset(file_pattern, class_file, size=416):
     files = tf.data.Dataset.list_files(file_pattern)
     dataset = files.flat_map(tf.data.TFRecordDataset)
 
-    #parse_tfrecord(dataset, class_table, size)
+    # parse_tfrecord(dataset, class_table, size)
 
     return dataset.map(lambda x: parse_tfrecord(x, class_table, size))
 
